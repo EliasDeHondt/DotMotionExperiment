05ca9fb73a1b71027d4037d069adfee85444cf44
EliasDeHondt <Elias.dehondt@outlook.com>
2024-12-01 17:29:10 +0100

    Refactor monitor settings and add sound playback functionality

--- OptimalSAT.py
+++ OptimalSAT.py
@@ -1,11 +1,13 @@
-from psychopy import visual, core, event, monitors              # pip install psychopy
-import numpy as np                                              # pip install numpy
-import pandas as pd                                             # pip install pandas
-import random                                                   # pip install random
-import os                                                       # pip install os
+from psychopy import visual, core, event, monitors              # type: ignore
+from psychopy import sound                                      # type: ignore
+import random                                                   # type: ignore
 
 # Settings
-MONITOR_USER = 'Elias'
+MONITOR_USER = 'Yenthe'
+monitor_settings = {
+    "Stef": {"width": 30, "distance": 40, "resolution": (1920, 1200)},
+    "Yenthe": {"width": 40, "distance": 50, "resolution": (2560, 1440)}
+}
 
 # Time constants
 FIXATION_DURATION = 1
@@ -27,12 +29,10 @@ MINIMUM_ACCURACY_BLOCK1 = 0.85
 MINIMUM_ACCURACY_BLOCK2 = 0.70
 
 # Monitor settings
-if MONITOR_USER == 'Stef':
-    monitor = monitors.Monitor('Stef', width=30, distance=40) 
-    monitor.setSizePix((1920, 1200))
-elif MONITOR_USER == 'Yenthe':
-    monitor = monitors.Monitor('Yenthe', width=40, distance=50) 
-    monitor.setSizePix((2560, 1440))
+if MONITOR_USER in monitor_settings:
+    settings = monitor_settings[MONITOR_USER]
+    monitor = monitors.Monitor(MONITOR_USER, width=settings["width"], distance=settings["distance"])
+    monitor.setSizePix(settings["resolution"])
 else:
     print('Monitor not found. Please add monitor settings.')
     core.quit()
@@ -62,12 +62,17 @@ instruction_6 = visual.ImageStim(win, image='Images/Instructions-6.png', pos=(0,
 # Trial
 def trial(coherence, correct_answer, askConfidence, giveFeedback, timePunishment_decision, timePunishment_confidence):
 
+    # Start muziek afspelen
+    start_music = sound.Sound('Audio/start_music.wav')  # Zorg ervoor dat je het pad naar je geluidsbestand correct hebt
+    start_music.play()
+
+    # Wacht tot de muziek afgelopen is
+    core.wait(start_music.getDuration())
+
     # Set dotMotion parameters
     dotMotion.coherence = coherence
-    if correct_answer == 'c':
-        dotMotion.dir = 180
-    elif correct_answer == 'n':
-        dotMotion.dir = 0
+    if correct_answer == 'c':  dotMotion.dir = 180
+    elif correct_answer == 'n': dotMotion.dir = 0
 
     # Confidence mapping
     key_mapping = {'1': 1, '2': 2, '3': 3, '8': 4, '9': 5, '0': 6}
@@ -121,174 +126,54 @@ def trial(coherence, correct_answer, askConfidence, giveFeedback, timePunishment
 
     # Time punishment
     timePunishment = 0
-    if accuracy == 0:
-        timePunishment += timePunishment_decision
-    if askConfidence and accuracy == 0:
-        timePunishment += timePunishment_confidence[int(response_confidence)-1]
-    if askConfidence and accuracy == 1:
-        timePunishment += timePunishment_confidence[6-int(response_confidence)]
+    if accuracy == 0: timePunishment += timePunishment_decision
+    if askConfidence and accuracy == 0: timePunishment += timePunishment_confidence[int(response_confidence)-1]
+    if askConfidence and accuracy == 1: timePunishment += timePunishment_confidence[6-int(response_confidence)]
 
     # Show time punishment
     if timePunishment > 0:
         punishmentIndicator = visual.TextStim(win, text="Straftijd: " + str(timePunishment), color='white')
         punishmentIndicator.draw()
         win.flip()
-
     core.wait(timePunishment)
-
     return accuracy
 
-    
+def run_block(instruction_image, n_trials, coherence, giveFeedback, askConfidence, timePunishment_decision, timePunishment_confidence, min_accuracy=None):
+    instruction_image.draw()
+    space.draw()
+    win.flip()
+    event.waitKeys(keyList=keys_instruction)
+
+    mean_accuracy = 0
+    while mean_accuracy < min_accuracy if min_accuracy else 0:
+        mean_accuracy = 0
+        for trial_number in range(n_trials):
+            correct_answer = random.choice(['c', 'n'])
+            trial_accuracy = trial(coherence=coherence, correct_answer=correct_answer, giveFeedback=giveFeedback, askConfidence=askConfidence, timePunishment_decision=timePunishment_decision, timePunishment_confidence=timePunishment_confidence)
+            mean_accuracy += trial_accuracy
+        mean_accuracy = mean_accuracy / n_trials
+
 # Block 1
-
-instruction_1.draw()
-space.draw()
-win.flip()
-event.waitKeys(keyList=keys_instruction)
-
-mean_accuracy = 0
-while mean_accuracy < MINIMUM_ACCURACY_BLOCK1:
-
-    mean_accuracy = 0
-
-    for trial_number in range(N_TRIALS_BLOCK1):
-
-        correct_answer = random.choice(['c', 'n'])
-        trial_accuracy = trial(coherence=COHERENCE_EASY, correct_answer=correct_answer, giveFeedback=True, askConfidence=False, timePunishment_decision=0, timePunishment_confidence = [0, 0, 0, 0, 0, 0])
-
-        mean_accuracy += trial_accuracy
-
-    mean_accuracy = mean_accuracy / N_TRIALS_BLOCK1
-
+run_block(instruction_1, N_TRIALS_BLOCK1, COHERENCE_EASY,
+        giveFeedback=True, askConfidence=False, timePunishment_decision=0, timePunishment_confidence=[0, 0, 0, 0, 0, 0], min_accuracy=MINIMUM_ACCURACY_BLOCK1)
 # Block 2
-
-instruction_2.draw()
-space.draw()
-win.flip()
-event.waitKeys(keyList=keys_instruction)
-
-mean_accuracy = 0
-while mean_accuracy < MINIMUM_ACCURACY_BLOCK2:
-
-    mean_accuracy = 0
-
-    for trial_number in range(N_TRIALS_BLOCK2):
-
-        correct_answer = random.choice(['c', 'n'])
-        trial_accuracy = trial(coherence=COHERENCE_DIFFICULT, correct_answer=correct_answer, giveFeedback=True, askConfidence=False, timePunishment_decision=0, timePunishment_confidence = [0, 0, 0, 0, 0, 0])
-
-        mean_accuracy += trial_accuracy
-
-    mean_accuracy = mean_accuracy / N_TRIALS_BLOCK2
-
+run_block(instruction_2, N_TRIALS_BLOCK2, COHERENCE_DIFFICULT,
+        giveFeedback=True, askConfidence=False, timePunishment_decision=0, timePunishment_confidence=[0, 0, 0, 0, 0, 0], min_accuracy=MINIMUM_ACCURACY_BLOCK2)
 # Block 3
-
-instruction_3.draw()
-space.draw()
-win.flip()
-event.waitKeys(keyList=keys_instruction)
-
-score = 0
-timer = core.Clock()
-
-while timer.getTime() < TIME_BLOCK3:
-
-    correct_answer = random.choice(['c', 'n'])
-    trial_accuracy = trial(coherence=COHERENCE_EASY, correct_answer=correct_answer, giveFeedback=False, askConfidence=False, timePunishment_decision=1, timePunishment_confidence = [0, 0, 0, 0, 0, 0])
-
-    score += trial_accuracy
-
-scoreIndicator = visual.TextStim(win, text="Score: " + str(score), color='white')
-scoreIndicator.draw()
-win.flip()
-core.wait(2)
-
+run_block(instruction_3, N_TRIALS_BLOCK1, COHERENCE_EASY,
+        giveFeedback=False, askConfidence=False, timePunishment_decision=1, timePunishment_confidence=[0, 0, 0, 0, 0, 0])
 # Block 4
-
-instruction_4.draw()
-space.draw()
-win.flip()
-event.waitKeys(keyList=keys_instruction)
-
-score = 0
-timer = core.Clock()
-
-while timer.getTime() < TIME_BLOCK4:
-
-    correct_answer = random.choice(['c', 'n'])
-    trial_accuracy = trial(coherence=COHERENCE_DIFFICULT, correct_answer=correct_answer, giveFeedback=False, askConfidence=True, timePunishment_decision=1, timePunishment_confidence = [0, 0.2, 0.4, 0.6, 0.8, 1])
-
-    score += trial_accuracy
-
-scoreIndicator = visual.TextStim(win, text="Score: " + str(score), color='white')
-scoreIndicator.draw()
-win.flip()
-core.wait(2)
-
+run_block(instruction_4, N_TRIALS_BLOCK2, COHERENCE_DIFFICULT,
+        giveFeedback=False, askConfidence=True, timePunishment_decision=1, timePunishment_confidence=[0, 0.2, 0.4, 0.6, 0.8, 1])
 # Main task 1
-
-instruction_5.draw()
-space.draw()
-win.flip()
-event.waitKeys(keyList=keys_instruction)
-
-score = 0
-timer = core.Clock()
-
-while timer.getTime() < TIME_MAIN:
-
-    correct_answer = random.choice(['c', 'n'])
-    trial_accuracy = trial(coherence=COHERENCE_DIFFICULT, correct_answer=correct_answer, giveFeedback=False, askConfidence=True, timePunishment_decision=2, timePunishment_confidence = [0, 0.2, 0.4, 0.6, 0.8, 1])
-
-    score += trial_accuracy
-
+run_block(instruction_5, N_TRIALS_BLOCK1, COHERENCE_DIFFICULT,
+        giveFeedback=False, askConfidence=True, timePunishment_decision=2, timePunishment_confidence=[0, 0.2, 0.4, 0.6, 0.8, 1])
 # Main task 2
-
-instruction_6.draw()
-space.draw()
-win.flip()
-event.waitKeys(keyList=keys_instruction)
-
-score = 0
-timer = core.Clock()
-
-while timer.getTime() < TIME_MAIN:
-
-    correct_answer = random.choice(['c', 'n'])
-    trial_accuracy = trial(coherence=COHERENCE_DIFFICULT, correct_answer=correct_answer, giveFeedback=False, askConfidence=True, timePunishment_decision=1, timePunishment_confidence = [0, 0.4, 0.8, 1.2, 1.6, 2])
-
-    score += trial_accuracy
-
+run_block(instruction_6, N_TRIALS_BLOCK1, COHERENCE_DIFFICULT,
+        giveFeedback=False, askConfidence=True, timePunishment_decision=1, timePunishment_confidence=[0, 0.4, 0.8, 1.2, 1.6, 2])
 # Main task 3
-
-instruction_6.draw()
-space.draw()
-win.flip()
-event.waitKeys(keyList=keys_instruction)
-
-score = 0
-timer = core.Clock()
-
-while timer.getTime() < TIME_MAIN:
-
-    correct_answer = random.choice(['c', 'n'])
-    trial_accuracy = trial(coherence=COHERENCE_DIFFICULT, correct_answer=correct_answer, giveFeedback=False, askConfidence=True, timePunishment_decision=2, timePunishment_confidence = [0, 0.2, 0.4, 0.6, 0.8, 1])
-
-    score += trial_accuracy
-
+run_block(instruction_6, N_TRIALS_BLOCK1, COHERENCE_DIFFICULT,
+        giveFeedback=False, askConfidence=True, timePunishment_decision=2, timePunishment_confidence=[0, 0.2, 0.4, 0.6, 0.8, 1])
 # Main task 4
-
-instruction_6.draw()
-space.draw()
-win.flip()
-event.waitKeys(keyList=keys_instruction)
-
-score = 0
-timer = core.Clock()
-
-while timer.getTime() < TIME_MAIN:
-
-    correct_answer = random.choice(['c', 'n'])
-    trial_accuracy = trial(coherence=COHERENCE_DIFFICULT, correct_answer=correct_answer, giveFeedback=False, askConfidence=True, timePunishment_decision=1, timePunishment_confidence = [0, 0.4, 0.8, 1.2, 1.6, 2])
-
-    score += trial_accuracy
+run_block(instruction_6, N_TRIALS_BLOCK1, COHERENCE_DIFFICULT,
+        giveFeedback=False, askConfidence=True, timePunishment_decision=1, timePunishment_confidence=[0, 0.4, 0.8, 1.2, 1.6, 2])
